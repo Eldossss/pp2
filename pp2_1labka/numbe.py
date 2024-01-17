014d66375591c5e71439abc6d6b223975d2b5262
Eldossss <142569353+Eldossss@users.noreply.github.com>
2024-01-17 16:30:25 +0600

    Add files via upload

---
+++ pp2_1labka/numbe.py
@@ -0,0 +1,13 @@
+#1
+
+x = 5
+x = float(x)
+print(x)
+#x  =5.0
+
+
+#2
+x = 5.5
+x = int(x)
+print(x) # x= 5
+
---
+++ pp2_1labka/strings.py
@@ -0,0 +1,43 @@
+#1
+
+x = "Helloo world"
+print(len(x))
+
+#2
+
+txt = "Hello world"
+x  = txt[0]
+
+#3
+
+txt  = "Helloo world"
+x = txt[2:5]
+
+
+#4
+
+txt = "Hello world"
+x = txt.strip()
+
+#5
+
+
+txt = "Hello world"
+txt = txt.upper()
+
+#6
+
+
+txt = "Hello world"
+txt = txt.lower()
+
+#7
+
+
+txt = "Hello world"
+txt = txt.replace("H","J")
+
+
+
+
+
