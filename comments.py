ce1ee7bc84ebeef954d69221d39f77e19be28fbb
Eldossss <142569353+Eldossss@users.noreply.github.com>
2024-01-17 16:28:07 +0600

    Add files via upload

---
+++ comments.py
@@ -0,0 +1,10 @@
+1#
+
+# This is a commentary 
+
+
+#2
+"""
+This is a comment  as well !!!
+
+"""
---
+++ datatypes.py
@@ -0,0 +1,42 @@
+#1
+
+x = 5
+print(type(x))
+
+#type = int
+
+#2
+
+x  ="Hello world"
+print(type(x))
+
+# type = str
+
+
+#3
+x = 20.5
+print(type(x))
+# type = float
+
+#4
+x  = ["apple", "banana" , "cherry"]
+print(type(x))
+
+# type = list
+
+#5
+x = ("apple", "banana", "Cherry")
+print(type(x))
+#type =  tuple
+
+#6
+x = {"name" : "John", "age"  : 36}
+print(type(x))
+#type  = dictionary
+
+#7
+x =True 
+print(type(x))
+#type = bool
+
+
